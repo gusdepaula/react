58363f6b85b0da85d56d7cadd86533b89ff64149
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-07-27 12:56:21 -0300

    #ImersaoReact Aula 1 - React Components e AluraFlix

---
+++ guflix/src/components/Menu/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../assets/img/logo_guFlix.png';
+import './Menu.css';
+import Button from '../Button'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/"> 
+                <img className="Logo" src={Logo}  alt="GuFlix Logo" />
+            </a>
+            <Button as="a" href="/" className="ButtonLink">Novo vídeo</Button>
+        </nav>
+    );
+}
+
+export default Menu;
