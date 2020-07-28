6cd093c1ecab3cac35a2260cb089f92026afed09
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-07-28 12:00:31 -0300

    #ImersaoReact Aula 2 - Roteamento e State

--- guflix/src/components/Menu/index.js
+++ guflix/src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo_guFlix.png';
 import './Menu.css';
 import Button from '../Button'
@@ -6,10 +7,10 @@ import Button from '../Button'
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/"> 
+            <Link to="/"> 
                 <img className="Logo" src={Logo}  alt="GuFlix Logo" />
-            </a>
-            <Button as="a" href="/" className="ButtonLink">Novo vídeo</Button>
+            </Link>
+            <Button as={Link} className="ButtonLink" to="cadastro/Video">Novo vídeo</Button>
         </nav>
     );
 }
