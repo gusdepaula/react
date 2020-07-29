9d2ced45ac1e433de66b9e77dd70bec09c0bf91f
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-07-29 17:40:09 -0300

    Melhoria na visualização da Página 404

--- guflix/src/pages/Pagina404/index.js
+++ guflix/src/pages/Pagina404/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PageDefault from '../../components/PageDefault';
 
-const Pagina404 = 
-
 function Pagina404() {
     return (
         <PageDefault>
-            <div>Página 404</div>
+            <h1>Página 404</h1>
         </PageDefault>
     );
 }
